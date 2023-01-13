11d739123e301426ef85ffde146fc0a7117b01fa
USER <USER@CIJIN>
2023-01-13 11:50:58 +0530

    Inheritance

---
+++ My_Maven/src/main/java/students/Students.java
@@ -0,0 +1,16 @@
+package students;
+
+public class Students {
+	public void add() {
+		System.out.println("hiii");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Students obj=new Students();
+		obj.add();
+		
+	}
+
+}
---
+++ My_Maven/src/main/java/students/Test.java
@@ -0,0 +1,15 @@
+package students;
+
+public class Test {
+	public void add()
+	{
+		System.out.println("Test");
+	}
+	public static void main(String[] args) {
+		Test sc=new Test();
+			sc.add();
+		// TODO Auto-generated method stub
+
+	}
+
+}
